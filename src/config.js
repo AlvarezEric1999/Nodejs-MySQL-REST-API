9da4be153bfca5ea76b5ef47c61820b551942e3e
Eric Alvarez <alvarezaeric592@gmail.com>
2023-06-09 19:22:02 -0500

    start script added

--- src/config.js
+++ src/config.js
@@ -8,5 +8,5 @@ export const dbPort = process.env.DB_PORT || 3306
 export const user = process.env.DB_USER || "root"
 export const host = process.env.DB_HOST || "localhost"
 export const password = process.env.DB_PASSWORD || "admin"
-export const database = process.env.DB_DATABASE || "Companydb"
+export const database = process.env.DB_DATABASE 
 
