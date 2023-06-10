0715c180743161d91068f1fa2709cf2101ea745a
Eric Alvarez <alvarezaeric592@gmail.com>
2023-06-09 22:59:54 -0500

    start script added

--- src/config.js
+++ src/config.js
@@ -3,10 +3,10 @@ import { config } from "dotenv";
 config()
 
 
-export const Port = process.env.PORT || 3000
-export const dbPort = process.env.DB_PORT || 3306
-export const user = process.env.DB_USER || "root"
-export const host = process.env.DB_HOST || "localhost"
-export const password = process.env.DB_PASSWORD || "admin"
+export const Port = process.env.PORT 
+export const dbPort = process.env.DB_PORT 
+export const user = process.env.DB_USER 
+export const host = process.env.DB_HOST 
+export const password = process.env.DB_PASSWORD 
 export const database = process.env.DB_DATABASE 
 
